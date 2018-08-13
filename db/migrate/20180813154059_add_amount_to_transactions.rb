ac45d0362efb6eb8f4b9ea8c1a9879b82ded5b43
Andre Sousa <sousa.andre@outlook.pt>
2018-08-13 16:58:05 +0100

    add columns

--- db/migrate/20180813154059_add_amount_to_transactions.rb
+++ db/migrate/20180813154059_add_amount_to_transactions.rb
@@ -1,5 +1,5 @@
 class AddAmountToTransactions < ActiveRecord::Migration[5.2]
   def change
-    add_column :transactions, :email, :integer
+    add_column :transactions, :amount, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_154811) do
+ActiveRecord::Schema.define(version: 2018_08_13_150406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,6 @@ ActiveRecord::Schema.define(version: 2018_08_13_154811) do
   create_table "transactions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "email"
-    t.string "description"
-    t.string "category"
   end
 
   create_table "user_subscriptions", force: :cascade do |t|

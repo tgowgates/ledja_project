42c5f87bc741ca0d928275fa2a85cc1cbaee1d69
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-15 10:21:02 +0100

    fixed api controller, added temp show page in pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Adding routes for Plaid API
   get 'plaid_api/new'
+  get 'pages/show', to: 'pages#show'
   post '/callback', to: 'plaid_api#callback'
 end

e4b5dd155cc59b40099abd03d7e1bf4fdc5ee406
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-16 22:49:40 +0100

    added account logic - need to tag user

--- app/controllers/plaid_api_controller.rb
+++ app/controllers/plaid_api_controller.rb
@@ -2,8 +2,20 @@ class PlaidApiController < ApplicationController
 
   def callback
     service = PlaidTransactionsService.new(params[:public_token])
-    filtered_transactions = service.subscription_transactions
+    plaid_data = service.request_plaid_data
+    # save account names
+    account_names = service.account_names(plaid_data)
+    save_account_names(account_names)
+    # save transactions
+    filtered_transactions = service.filter_transactions(plaid_data)
     save_filtered_transactions(filtered_transactions)
+  end
+
+  def save_account_names(account_names)
+    account_names.each do |account_name|
+      new_account = Account.new(name: account_name)
+      new_account.save!
+    end
   end
 
   def save_filtered_transactions(filtered_transactions)
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,3 @@
 class Account < ApplicationRecord
   has_many :transactions
-  belongs_to :plaid_item
 end

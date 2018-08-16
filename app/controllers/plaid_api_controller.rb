8a59e581d637c5d78a5ecb9a543d9c353b3c6eee
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-16 12:25:33 +0100

    testing in dev

--- app/controllers/plaid_api_controller.rb
+++ app/controllers/plaid_api_controller.rb
@@ -3,7 +3,6 @@ class PlaidApiController < ApplicationController
   def callback
     service = PlaidTransactionsService.new(params[:public_token])
     filtered_transactions = service.subscription_transactions
-    byebug
     save_filtered_transactions(filtered_transactions)
   end
 
@@ -15,7 +14,6 @@ class PlaidApiController < ApplicationController
         category: transaction['transaction_id'],
         user_id: current_user )
       new_transaction.save!
-      byebug
     end
   end
 

397e4d7b0f16bb5da9ec91794e40e3419388e627
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-16 17:12:28 +0100

    added field to user db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2018_08_16_144915) do
+
+ActiveRecord::Schema.define(version: 2018_08_16_161200) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,7 +87,7 @@ ActiveRecord::Schema.define(version: 2018_08_16_144915) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "first_run"
+    t.date "api_last_run"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

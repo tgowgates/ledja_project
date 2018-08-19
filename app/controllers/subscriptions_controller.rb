29270b0868dc65ef8288ec40b74d7162cc39b546
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-19 14:46:34 +0100

    fixed page reload bug

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -13,13 +13,14 @@ class SubscriptionsController < ApplicationController
       #next payment: last transaction date + 1 month
 
     #array of unique subsription names for current_user
-    @descriptions = Transaction.where(user: @current_user).pluck(:description).uniq
-    #hash (description => last transaction) with last transaction for each subscription for current user
-    @lastTransactions = []
-    @descriptions.each do |desc|
-      @lastTransactions << Transaction.where("user_id = ? AND description = ?", @current_user.id, desc).order('date DESC').limit(1)[0]
+    if current_user.api_last_run != nil
+      @descriptions = Transaction.where(user: @current_user).pluck(:description).uniq
+      #hash (description => last transaction) with last transaction for each subscription for current user
+      @lastTransactions = []
+      @descriptions.each do |desc|
+        @lastTransactions << Transaction.where("user_id = ? AND description = ?", @current_user.id, desc).order('date DESC').limit(1)[0]
+      end
     end
-
     # @lastTransactions
 
     #exemple: you want amount   for last trans with mcdonald @lastTransaction["mcdonald"].ammount

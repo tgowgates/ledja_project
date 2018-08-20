b99b503888eaf4e425bada39b7c049797b5b5dad
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-20 19:29:53 +0100

    fixing seeding and category cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,20 @@ SubscriptionPackage.destroy_all
 SubscriptionPackage.create(
     name: "Netflix Basic",
     category: "Netflix",
-    price: 7.99
+    price: 7.99,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784378/ACSszfFzO4XqUIBEMm4bLoIbmAWT7-NyiFTgjHi22Q_s900-mo-c-c0xffffffff-rj-k-no.jpg",
     url: "https://www.netflix.com/ChangePlan"
   )
  SubscriptionPackage.create(
     name: "Hulu Commercials",
     category: "Netlfix",
-    price: 7.99
+    price: 7.99,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784342/nxa4lEyS_400x400.jpg"
   )
 SubscriptionPackage.create(
     name: "Amazon Video",
     category: "Netflix",
-    price: 0
+    price: 0,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784400/Uq2Q-ded_400x400.jpg"
   )
 
@@ -32,20 +32,20 @@ SubscriptionPackage.create(
 SubscriptionPackage.create(
     name: "Hulu Commercials",
     category: "Hulu",
-    price: 7.99
+    price: 7.99,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784342/nxa4lEyS_400x400.jpg"
   )
  SubscriptionPackage.create(
     name: "Netflix Basic",
     category: "Hulu",
-    price: 7.99
+    price: 7.99,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784378/ACSszfFzO4XqUIBEMm4bLoIbmAWT7-NyiFTgjHi22Q_s900-mo-c-c0xffffffff-rj-k-no.jpg",
     url: "https://www.netflix.com/ChangePlan"
   )
 SubscriptionPackage.create(
     name: "HBO w/o TV",
     category: "Hulu",
-    price: 14.99
+    price: 14.99,
     photo: "https://res.cloudinary.com/dzcwxfufs/image/upload/v1534784428/hbo-logo.jpg"
   )
 

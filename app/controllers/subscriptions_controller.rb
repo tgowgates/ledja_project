db9c08d7e91bc6fd26c9596eb24efbd689493880
Andre Sousa <sousa.andre@outlook.pt>
2018-08-20 09:55:57 +0100

    acconts view

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 class SubscriptionsController < ApplicationController
   def index
     @current_user = current_user
+    @accounts = Account.all
     # figure out if the user is running for first time
     # first run true/false
   end

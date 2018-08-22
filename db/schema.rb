e1a470932856b92606dbb7e2ce732cda7b7251e3
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-22 12:26:21 +0100

    changed table layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_173650) do
+ActiveRecord::Schema.define(version: 2018_08_22_111602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_173650) do
     t.boolean "reminder"
     t.boolean "deal_notification"
     t.string "name"
+    t.boolean "cancelled"
     t.index ["subscription_package_id"], name: "index_user_subscriptions_on_subscription_package_id"
     t.index ["user_id"], name: "index_user_subscriptions_on_user_id"
   end
